28bbc273ec5167967bd44df45fa9c8a4cfb64f28
Foqia-Sd <160354822+Foqia-Sd@users.noreply.github.com>
2025-12-06 20:14:35 +0500

    bug resolve

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -21,7 +21,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
+          editUrl: 'https://github.com/Foqia-Sd/physical-ai-textbook/edit/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
